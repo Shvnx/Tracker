08f3bacd2c644505685590097f8a81cf0836739f
Shvnx <xcallius3@gmail.com>
2026-06-26 15:12:48 +0530

    Update platforms.js

--- src/platforms.js
+++ src/platforms.js
@@ -1,16 +1,20 @@
-async function fetchInstagram(token, postId) {
-  let id = postId;
-  if (!id) {
-    const r = await fetch(`https://graph.instagram.com/me/media?fields=id&limit=1&access_token=${token}`);
-    const j = await r.json();
-    if (j.error) throw new Error(j.error.message);
-    id = j.data[0].id;
-  }
-  const r = await fetch(`https://graph.instagram.com/${id}?fields=id,like_count,comments_count,media_type,timestamp&access_token=${token}`);
+async function fetchInstagram(rapidApiKey, postUrl) {
+  const url = `https://instagram-scraper-stable-api.p.rapidapi.com/get_media_data.php?reel_post_code_or_url=${encodeURIComponent(postUrl)}&type=reel`;
+  const r = await fetch(url, {
+    headers: {
+      'x-rapidapi-host': 'instagram-scraper-stable-api.p.rapidapi.com',
+      'x-rapidapi-key': rapidApiKey
+    }
+  });
   const j = await r.json();
-  if (j.error) throw new Error(j.error.message);
-  return { views: 0, likes: j.like_count || 0, comments: j.comments_count || 0 };
+  if (j.error) throw new Error(j.error);
+  return {
+    views: j.video_play_count || j.video_view_count || 0,
+    likes: j.like_count || 0,
+    comments: j.comment_count || 0
+  };
 }
+
 async function fetchTwitter(token, tweetId) {
   if (tweetId) {
     const r = await fetch(`https://api.twitter.com/2/tweets/${tweetId}?tweet.fields=public_metrics`, {
@@ -23,18 +27,18 @@ async function fetchTwitter(token, tweetId) {
   }
   const ur = await fetch('https://api.twitter.com/2/users/me', { headers: { Authorization: 'Bearer ' + token } });
   const uj = await ur.json();
-  if (uj.errors) throw new Error(uj.errors[0].detail);
   const tr = await fetch(`https://api.twitter.com/2/users/${uj.data.id}/tweets?max_results=5&tweet.fields=public_metrics`, {
     headers: { Authorization: 'Bearer ' + token }
   });
   const tj = await tr.json();
-  if (tj.errors) throw new Error(tj.errors[0].detail);
   const m = tj.data[0].public_metrics;
   return { views: m.impression_count || 0, likes: m.like_count || 0, comments: m.reply_count || 0 };
 }
+
 async function fetchSnapshot(platform, token, postId) {
   if (platform === 'instagram') return fetchInstagram(token, postId);
   if (platform === 'twitter') return fetchTwitter(token, postId);
   throw new Error('Unsupported platform: ' + platform);
 }
+
 module.exports = { fetchSnapshot };
